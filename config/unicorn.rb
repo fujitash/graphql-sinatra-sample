232c2f2bed345ac637e8ce40f8d41612a76f035d
fujitash <fujitash97@gmail.com>
2018-02-28 16:42:20 +0000

    fix path of log/pid/app_root

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,15 +1,10 @@
-sinatra_root = "/home/vagrant/helloworld"
-working_directory sinatra_root
-# working_directory "/var/www/unit/vagrant"
+working_directory "/var/www/unit/vagrant/graphql-sinatra-sample"
 
 worker_processes 2
 timeout 300
 
 listen "/tmp/unit-vagrant.socket"
-# listen "/tmp/nginx-unicorn.socket"
-pid "/run/unicorn/unit.pid"
+pid "/run/unicorn/unit-vagrant.pid"
 
-stderr_path "/var/log/app/unicorn.stderr.log"
-stdout_path "/var/log/app/unicorn.stdout.log"
-# stderr_path "/var/log/www/unicorn.stderr.log"
-# stdout_path "/var/log/www/unicorn.stdout.log"
+stdout_path "/var/log/www/unit/vagrant/unicorn.stdout.log"
+stderr_path "/var/log/www/unit/vagrant/unicorn.stderr.log"
